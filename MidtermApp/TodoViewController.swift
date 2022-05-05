cbde695ead171889c032e79635b8aef94e2c8493
jasjahan <mjahan@mail.sfsu.edu>
2022-05-05 14:21:27 -0700

    Updated 2 Functions
    Minor Change

--- MidtermApp/TodoViewController.swift
+++ MidtermApp/TodoViewController.swift
@@ -4,16 +4,10 @@ struct Store {
     var elements: [String] = ["Buy milk", "Feed cat", "Goat"]
 }
 
-//struct Store {
-//    let elements: String
-//}
-
 class TodoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
-//    var elementItems : [Store] = [Store(elements: "Buy milk"),
-//                                          Store(elements: "Feed Cat")]
-    
+
     let store = Store()
     
     
@@ -25,11 +19,6 @@ class TodoViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-//        let row = indexPath.row
-//        let a = elementItems[row]
-//        let name = a.elements
-//        cell.textLabel?.text = name
-        
         cell.textLabel?.text = store.elements[indexPath.row]
         
         return cell
@@ -37,7 +26,7 @@ class TodoViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return store.elements.count
-//        return elementItems.count
+     
     }
 
 }
